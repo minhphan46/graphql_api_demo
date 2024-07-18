62c981f891fb099b2f0a6e70d42b4829d9bc8c18
Phan Van Minh <21522345@gm.uit.edu.vn>
2024-07-18 09:59:45 +0700

    part 1

---
+++ Schema/Query.cs
@@ -0,0 +1,7 @@
+﻿namespace GraphQL.API.Schema
+{
+    public class Query
+    {
+        public string Instructions => "Smash that like button and subscribe to SingletonSean";
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,3 +1,4 @@
+using GraphQL.API.Schema;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -16,7 +17,7 @@ namespace GraphQL.API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddGraphQLServer();
+            services.AddGraphQLServer().AddQueryType<Query>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
